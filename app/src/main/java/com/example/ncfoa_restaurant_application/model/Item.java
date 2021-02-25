219119219af88cbfc18fcef0485e4a0d94a19e5c
Tagore Hari Ravindra <tagorehyd@live.co.uk>
2021-02-25 17:01:31 +0530

    added model classes

---
+++ app/src/main/java/com/example/ncfoa_restaurant_application/model/Item.java
@@ -0,0 +1,59 @@
+package com.example.ncfoa_restaurant_application.model;
+
+public class Item {
+    private Integer id;
+    private String name;
+    private String description;
+    private Double price;
+
+    public Boolean getAvailabe() {
+        return availabe;
+    }
+
+    public void setAvailabe(Boolean availabe) {
+        this.availabe = availabe;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    private Boolean availabe;
+    private Integer rating;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
